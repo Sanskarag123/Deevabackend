bfb4b38aeeeb3ddebe29a2a97686dccee01d6f7c
unknown <agrawalsanskar9993@gmail.com>
2021-05-18 15:12:50 +0530

    :bug:dburl

--- services/connection.js
+++ services/connection.js
@@ -1,5 +1,5 @@
 const MongoClient = require( 'mongodb' ).MongoClient;
-const url = "mongodb://localhost:27017/deeva";
+const url = "mongodb+srv://san:1234@cluster0.nmxs5.mongodb.net/deeva?retryWrites=true&w=majority";
 
 var _db =  MongoClient.connect( url,  { useNewUrlParser: true,useUnifiedTopology:true }) ;
 
